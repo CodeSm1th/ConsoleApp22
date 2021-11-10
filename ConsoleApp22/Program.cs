5f381aa3858c19aa23ff4da34060439bfe86e1f2
Mathias Hansen <maha38710@edu.ucl.dk>
2021-11-10 10:15:20 +0100

    yes

--- ConsoleApp22/Program.cs
+++ ConsoleApp22/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp22
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bruh");
+
         }
     }
 }
